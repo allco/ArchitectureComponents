e1bb1b3b7eba1ad6f9b57ddc798ef0d9d74964df
pawel bochenski <pawel.bochenski@siili.com>
2017-07-12 22:33:57 +0200

    fixed unit test for LiveData

--- app/src/test/java/pl/pbochenski/archcomponentstest/MainViewModelTest.kt
+++ app/src/test/java/pl/pbochenski/archcomponentstest/MainViewModelTest.kt
@@ -1,12 +1,11 @@
 package pl.pbochenski.archcomponentstest
 
+import android.arch.core.executor.testing.InstantTaskExecutorRule
 import android.arch.lifecycle.MutableLiveData
-import com.nhaarman.mockito_kotlin.doReturn
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.stub
-import com.nhaarman.mockito_kotlin.verify
-import junit.framework.Assert
+import android.arch.lifecycle.Observer
+import com.nhaarman.mockito_kotlin.*
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import pl.pbochenski.archcomponentstest.hackernews.Item
 import pl.pbochenski.archcomponentstest.hackernews.Type
@@ -14,16 +13,30 @@ import pl.pbochenski.archcomponentstest.hackernews.Type
 /**
  * Created by Pawel Bochenski on 29.06.2017.
  */
+typealias ViewModelReturnType = Pair<List<ItemData>, List<ItemData>>
+
 class MainViewModelTest {
 
     val repo = mock<PostRepo>()
+    lateinit var liveData: MutableLiveData<List<Item>>
     lateinit var model: MainViewModel
+
+    @Rule
+    @JvmField
+    val rule = InstantTaskExecutorRule()
 
     @Before
     fun before() {
+        reset(repo)
         val app = mock<App> {
             on { postRepo } doReturn (repo)
         }
+
+        liveData = MutableLiveData<List<Item>>()
+        repo.stub {
+            on { getPosts() } doReturn liveData
+        }
+
         model = MainViewModel(app)
     }
 
@@ -33,16 +46,24 @@ class MainViewModelTest {
         verify(repo).load(15)
     }
 
-    @Test
-    fun shouldGetPostAndHaveACopyOfIt() {
-        val toReturn = MutableLiveData<List<Item>>().apply {
-            value = listOf(Item(1, false, Type.story, "", 123, "123", false, null, null, emptyList(), null, 1, "test", null, null))
-        }
-        repo.stub {
-            on { getPosts() } doReturn toReturn
-        }
-        val value = model.getPosts().value
 
-        Assert.assertEquals(toReturn, value)
+    @Test
+    fun shouldGetPostsAndHaveACopyOfIt() {
+        //given
+        val returnedItem = createDummyItem()
+        val observer = mock<Observer<ViewModelReturnType>>()
+        model.getPosts().observeForever(observer)
+
+        //when
+        liveData.value = listOf(returnedItem)
+
+        //than
+        verify(observer).onChanged(
+                ViewModelReturnType(
+                        emptyList(),
+                        listOf(ItemData.Post(Post(returnedItem.id, returnedItem.title, returnedItem.url)),
+                                ItemData.Spinner)))
     }
+
+    private fun createDummyItem() = Item(1, false, Type.story, "", 123, "123", false, null, null, emptyList(), null, 1, "test", null, null)
 }
